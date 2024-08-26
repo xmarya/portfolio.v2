f57249a4de14463c889fded6260281937f68f8b5
xmarya <marya_hy@hotmail.com>
2024-08-26 17:44:00 +0300

    a littel bit until finishing the offers section

--- src/sections/Offer.jsx
+++ src/sections/Offer.jsx
@@ -1,6 +1,40 @@
+import styled from "styled-components";
 import AnimatedWrapper from "../UI/AnimatedWrapper";
-import { SectionHeading } from "../UI/Headings";
+import { SectionHeading, SectionSubHeading } from "../UI/Headings";
 import { Section } from "../UI/Section";
+
+
+const OffersList = styled.ul`
+  list-style-position: inside;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+  padding: 2rem 7rem;
+`;
+
+const OffersListItems = styled.li`
+/* making each item the same width in order to make the correctley aligned by setting  justify-content: start*/
+  width: 20rem;
+  text-align: start;
+  font-size: var(--lg-text);
+  text-transform: capitalize;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+
+  background-color: grey;
+
+  &::before {
+    content: "";
+    position: relative;
+    display: block;
+    background-color: var(--neon-purple);
+    width: 1rem;
+    height: 1rem;
+    margin-right: 0.5rem;
+  }
+`;
+
 
 export default function Offer() {
   return (
@@ -28,6 +62,17 @@ export default function Offer() {
           application.
         </p>
       </AnimatedWrapper>
+
+      <div className="bg-black w-[50%]">
+        {/* <SectionSubHeading>My Services:</SectionSubHeading> */}
+        <ul className="bg-purple-600 list-inside flex flex-wrap items-start justify-between gap-8 py-8 px-28">
+          <OffersListItems>UI/UX Design</OffersListItems>
+          <OffersListItems>Wireframe Design</OffersListItems>
+          <OffersListItems>front-end Development</OffersListItems>
+          <OffersListItems>Back-end Development</OffersListItems>
+        </ul>
+      </div>
+      
     </Section>
   );
 }
